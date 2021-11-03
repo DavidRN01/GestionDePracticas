20946ec3eac80cb0791eb1ea8ab06fa4a568a7f5
DavidRamosNavas <DavidRamosNavas@CS-EST-PC-13145>
2021-11-03 10:22:16 +0100

    Avances con las vistas

---
+++ src/main/java/com/mycompany/gestiondepracticas/EditarTareaController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gestiondepracticas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+
+/**
+ * FXML Controller class
+ *
+ * @author DavidRamosNavas
+ */
+public class EditarTareaController implements Initializable {
+
+    @FXML
+    private Spinner<?> horas;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
