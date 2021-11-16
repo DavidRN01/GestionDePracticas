f50ec1300195c58c0ffe2401533e1c3ce267372c
Alejandro Aguilar <alexakarex@gmail.com>
2021-11-16 22:16:53 +0100

    Editar alumno terminado y nuevo alumno falta corregir errores

--- src/main/java/com/mycompany/gestiondepracticas/NuevoAlumnoController.java
+++ src/main/java/com/mycompany/gestiondepracticas/NuevoAlumnoController.java
@@ -1,27 +1,87 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.gestiondepracticas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import models.Alumno;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-/**
- * FXML Controller class
- *
- * @author DavidRamosNavas
- */
 public class NuevoAlumnoController implements Initializable {
+
+    @FXML
+    private TextField labelNombre;
+    @FXML
+    private TextField labelApellidos;
+    @FXML
+    private TextField labelCorreo;
+    @FXML
+    private TextField labelPass;
+    @FXML
+    private TextField labelDNI;
+    @FXML
+    private DatePicker dateFecha;
+    @FXML
+    private TextField labelTelefono;
+    @FXML
+    private TextField labelDual;
+    @FXML
+    private TextField labelFCT;
+    @FXML
+    private Button btnAceptar;
+    @FXML
+    private Button btnCancelar;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+    }
+
+    @FXML
+    private void aceptar(ActionEvent event) {
+
+        Alumno a = new Alumno();
+
+        a.setId(0L);
+        a.setNombre(labelNombre.getText());
+        a.setApellidos(labelApellidos.getText());
+        a.setEmail(labelCorreo.getText());
+        a.setContraseña(labelPass.getText());
+        a.setDni(labelDNI.getText());
+        a.setFecha_nacimiento(java.sql.Date.valueOf(dateFecha.getValue()));
+        a.setTelefono(Integer.parseInt(labelTelefono.getText()));
+        a.setHoras_dual(Double.parseDouble(labelDual.getText()));
+        a.setHoras_fct(Double.parseDouble(labelFCT.getText()));
+        
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction tr = s.beginTransaction();
+        s.save(a);
+        tr.commit();
+
+        try {
+            App.setRoot("profesor");
+        } catch (IOException ex) {
+            Logger.getLogger(NuevoAlumnoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+        try {
+            App.setRoot("profesor");
+        } catch (IOException ex) {
+            Logger.getLogger(NuevoAlumnoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
